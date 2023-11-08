1964389380659bdfa86ce0b660b25e5cc1f7b473
thienkhoalewlew <thienkhoalewlew@gmail.com>
2023-11-09 03:03:20 +0700

    3h03

--- Views/script.js
+++ Views/script.js
@@ -9,3 +9,12 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+function openFileUploader() {
+    document.getElementById('file-upload').click();
+}
+
+document.getElementById('file-upload').addEventListener('change', function() {
+    var fileName = this.value.split('\\').pop();
+    document.getElementById('file-label').innerText = fileName || 'Choose a file';
+});
