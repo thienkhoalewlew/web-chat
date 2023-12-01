2f9967beea87203f1d2b348e1b0148168e5e5a9f
thienkhoalewlew <thienkhoalewlew@gmail.com>
2023-12-01 09:11:37 +0700

    add nodejs + socket.io + check email & password

--- Views/login.js
+++ Views/login.js
@@ -31,3 +31,35 @@ iconCloseReset.addEventListener("click", () => {
   formLoginRegister.classList.remove("active-show-reset");
   formReset.classList.remove("active-hidden-login");
 });
+
+
+const loginForm = document.querySelector('#loginForm');
+
+loginForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+
+  try {
+    const response = await fetch('http://localhost:3000/authenticate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      const userId = data.userId;
+      // Chuyển đến trang main.html với userId
+      window.location.href = `main.html?userId=${userId}`;
+    } else {
+      res.status(401).json({ message: 'Email hoặc password không đúng' });
+    }
+  } catch (error) {
+    console.error('Lỗi:', error);
+  }
+});
